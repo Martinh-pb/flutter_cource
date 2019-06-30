02c60e5125dcbfeb2338f1bf7de760efc80e440f
Martin Haisma <martinhaisma@gmail.com>
2019-06-30 23:15:29 +0200

    Edit product and delete products

--- lib/pages/product_edit.dart
+++ lib/pages/product_edit.dart
@@ -1,17 +1,21 @@
 import 'package:flutter/material.dart';
 
-class ProductCreatePage extends StatefulWidget {
+class ProductEditPage extends StatefulWidget {
   final Function addProduct;
+  final Function updateProduct;
+  final Map<String, dynamic> product;
+  final int productIndex;
 
-  ProductCreatePage(this.addProduct);
+  ProductEditPage(
+      {this.addProduct, this.updateProduct, this.product, this.productIndex});
 
   @override
   State<StatefulWidget> createState() {
-    return _ProductCreatePageState();
+    return _ProductEditPageState();
   }
 }
 
-class _ProductCreatePageState extends State<ProductCreatePage> {
+class _ProductEditPageState extends State<ProductEditPage> {
   Map<String, dynamic> _formData = {
     'title': null,
     'description': null,
@@ -24,6 +28,7 @@ class _ProductCreatePageState extends State<ProductCreatePage> {
   Widget _buildProductTitleTextField() {
     return TextFormField(
       decoration: InputDecoration(labelText: 'Product title'),
+      initialValue: widget.product == null ? '' : widget.product['title'],
       validator: (String value) {
         if (value.isEmpty || value.length < 5) {
           return 'Product title is required and should be longer than 5 characters.';
@@ -38,6 +43,8 @@ class _ProductCreatePageState extends State<ProductCreatePage> {
   Widget _buildDescriptionTextField() {
     return TextFormField(
         decoration: InputDecoration(labelText: 'Product description'),
+        initialValue:
+            widget.product == null ? '' : widget.product['description'],
         maxLines: 4,
         validator: (String value) {
           if (value.isEmpty || value.length < 10) {
@@ -52,6 +59,8 @@ class _ProductCreatePageState extends State<ProductCreatePage> {
   Widget _buildPriceTextField() {
     return TextFormField(
         decoration: InputDecoration(labelText: 'Product price'),
+        initialValue:
+            widget.product == null ? '' : widget.product['price'].toString(),
         keyboardType: TextInputType.number,
         validator: (String value) {
           if (value.isEmpty ||
@@ -64,18 +73,8 @@ class _ProductCreatePageState extends State<ProductCreatePage> {
         });
   }
 
-  void submitForm() {
-    if (!_formKey.currentState.validate()) {
-      return;
-    }
-    _formKey.currentState.save();
-
-    widget.addProduct(_formData);
-    Navigator.pushReplacementNamed(context, '/products');
-  }
-
-  @override
-  Widget build(BuildContext context) {
+  Widget _buildPageContent(BuildContext context)
+  {
     final double deviceWidth = MediaQuery.of(context).size.width;
     final double targetWidth = deviceWidth > 768.0 ? 700.0 : deviceWidth * 0.95;
     final double paddingWidth = deviceWidth - targetWidth;
@@ -107,5 +106,32 @@ class _ProductCreatePageState extends State<ProductCreatePage> {
         ),
       ),
     );
+  }
+
+  void submitForm() {
+    if (!_formKey.currentState.validate()) {
+      return;
+    }
+    _formKey.currentState.save();
+    if (widget.product == null) {
+      widget.addProduct(_formData);
+    } else {
+      widget.updateProduct(widget.productIndex, _formData);
+    }
+    Navigator.pushReplacementNamed(context, '/products');
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final Widget pageContent = _buildPageContent(context);
+
+    return widget.product == null
+        ? pageContent
+        : Scaffold(
+            appBar: AppBar(
+              title: Text('Edit product'),
+            ),
+            body: pageContent,
+          );
   }
 }
--- lib/pages/product_list.dart
+++ lib/pages/product_list.dart
@@ -1,13 +1,56 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_course/pages/product_edit.dart';
 
 class ProductListPage extends StatelessWidget {
   final Function deleteProduct;
+  final Function updateProduct;
   final List<Map<String, dynamic>> products;
-  
-  ProductListPage(this.deleteProduct, this.products);
-  
+
+  ProductListPage({this.updateProduct, this.deleteProduct, this.products});
+
+  Widget _buildEditButton(BuildContext context, int index) {
+    return IconButton(
+        icon: Icon(Icons.edit),
+        onPressed: () {
+          Navigator.push(
+              context,
+              MaterialPageRoute(
+                  builder: (BuildContext context) => ProductEditPage(
+                        updateProduct: this.updateProduct,
+                        product: products[index],
+                        productIndex: index,
+                      )));
+        });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Center(child: Text('List of my products'),);
+    return ListView.builder(
+      itemBuilder: (BuildContext context, int index) {
+        return Dismissible(
+          key: Key(products[index]['title']),
+          background: Container(color: Colors.red),
+          onDismissed: (DismissDirection direction) {
+            if (direction == DismissDirection.endToStart) {
+              deleteProduct(index);
+            }
+          },
+          child: Column(
+            children: <Widget>[
+              ListTile(
+                leading: CircleAvatar(
+                  backgroundImage: AssetImage(products[index]['image']),
+                ),
+                title: Text(products[index]['title']),
+                subtitle: Text('\$${products[index]['price'].toString()}'),
+                trailing: _buildEditButton(context, index),
+              ),
+              Divider(),
+            ],
+          ),
+        );
+      },
+      itemCount: products.length,
+    );
   }
 }
